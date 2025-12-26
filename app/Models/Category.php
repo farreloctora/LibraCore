173ffecf745827fdbcca79125e8d0ab710236416
renuputra <renuresdinaputra@gmail.com>
2025-12-26 17:25:24 +0700

    feat: Model Category

---
+++ app/Models/Category.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'icon_color',
+    ];
+
+    /**
+     * Get the koleksis for the category.
+     */
+    public function koleksis(): HasMany
+    {
+        return $this->hasMany(Koleksi::class);
+    }
+}
+
